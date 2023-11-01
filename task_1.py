d1845a18c7631bff9b0a4b88f24740932d6825a6
InGOwned <103064059+InGOwned@users.noreply.github.com>
2023-11-01 15:03:02 +0000

    .

---
+++ task_1.py
@@ -0,0 +1,6 @@
+class Human:
+    def __init__(self, name, age):
+        self.default_name = name
+        self.default_age = age
+        # self.money = money
+        # self.house = having_a_house
