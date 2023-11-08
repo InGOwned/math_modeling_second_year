3c335daa509bfab53f94ac55bf6fbc2b9eff5ed2
InGOwned <103064059+InGOwned@users.noreply.github.com>
2023-11-08 15:25:26 +0000

    Доделал задачу

--- task_1.py
+++ task_1.py
@@ -9,10 +9,11 @@ class Human:
         self._house = "Сейчас дома нет"
     
     def info(self):
+        print('\nСведения о человеке')
         print(f'Имя: {self.name}')
         print(f'Возраст: {self.age}')
-        print(f"Кол-во денег: {self._money}")
-        print(self._house)
+        print(f"Кол-во денег: {self._money} руб.")
+        print(self._house, '\n')
 
     @staticmethod
     def default_info():
@@ -20,40 +21,50 @@ class Human:
         print(Human.default_age)
     
     def _make_deal(self, house):
-        self._money -= house._price
-        self._house = house
+        self._money -= house.get_price()
+        self._house = f'Имеется {house.condition()} стоимостью {house.get_price()} рублей'
     
     def earn_money(self, amount):
+        print(f'Человек получил на счёт {amount} рублей\n')
         self._money += amount
     
     def buy_house(self, house):
-        if house._price > self._money:
-            print("Денег на дом нема")
+        if house.get_price() > self._money:
+            print("Попытка купить дом не удалась. Денег на дом нема\n")
         else:
             print("Денег хватает, дом приобритён!")
             self._make_deal(house)
 
 
 class House:
-    def __init__(self, area, price):
+    def __init__(self, area, price, condition="обычный дом"):
         self._area = area
         self._price = price
+        self.__condition = condition
+        # self._address = address
     
     def final_price(self, discount):
         self._price -= (discount / 100) * self._price
-
+    
+    def get_price(self):
+        return self._price
+    
+    def condition(self):
+        return self.__condition
 
 class SmallHouse(House):
     def __init__(self, price):
-        super().__init__(area=40, price=price)
-
+        super().__init__(area=40, price=price, condition='маленький дом')
 
 Human.default_info()
 
-human = Human('Misha', 27)
+human = Human('Валера', 27)
 human.info()
 
 house = SmallHouse(4000000)
 human.buy_house(house)
 
+human.earn_money(3000000)
+human.buy_house(house)
 
+human.info()
