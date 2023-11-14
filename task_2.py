2979c87ac798e15e44ab7539b18e217ead59c7d9
InGOwned <103064059+InGOwned@users.noreply.github.com>
2023-11-14 17:39:45 +0000

    task_3.py

--- task_2.py
+++ task_2.py
@@ -1,20 +1,18 @@
 class Cell:
-    def __init__(self, num_cells):
-        self.num_cells = num_cells
+    def __init__(self, num_of_cells):
+        self.num_of_cells = num_of_cells
 
     def __add__(self, other):
-        return Cell(self.num_cells + other.num_cells)
+        return Cell(self.num_of_cells + other.num_of_cells)
 
     def __sub__(self, other):
-        if self.num_cells < other.num_cells:
-            return ("Разность клеток не может быть отрицательной")
-        return Cell(self.num_cells - other.num_cells)
+        return Cell(self.num_of_cells - other.num_of_cells)
 
     def __mul__(self, other):
-        return Cell(self.num_cells * other.num_cells)
+        return Cell(self.num_of_cells * other.num_of_cells)
 
     def __truediv__(self, other):
-        return Cell(round(self.num_cells / other.num_cells))
+        return Cell(round(self.num_of_cells / other.num_of_cells))
 
 cell1 = Cell(12)
 cell2 = Cell(5)
@@ -24,7 +22,7 @@ cell4 = cell1 - cell2
 cell5 = cell1 * cell2
 cell6 = cell1 / cell2
 
-print(cell3.num_cells)
-print(cell4.num_cells)
-print(cell5.num_cells)
-print(cell6.num_cells)
+print(cell3.num_of_cells)
+print(cell4.num_of_cells)
+print(cell5.num_of_cells)
+print(cell6.num_of_cells)
---
+++ task_3.py
@@ -0,0 +1,102 @@
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def __len__(self):
+        return int((self.x**2 + self.y**2 + self.z**2)**(1/2))
+    
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
+    
+    def __repr__(self):
+        return f"Vector({self.x}, {self.y}, {self.z})"
+    
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        return "Складывать можно только вектор с вектором"
+    
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+        return "Вычитать можно только вектор из вектора"
+    
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y + self.z * other.z
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other, self.z * other)
+        return ("Умножать можно только вектор на число или вектор")
+    
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        self.z += other.z
+        return self
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        self.z -= other.z
+        return self
+
+    def __imul__(self, other):
+        self.x *= other
+        self.y *= other
+        self.z *= other
+        return self
+    
+    def __eq__(self, other):
+        if isinstance(other, Vector):
+            return self.x == other.x and self.y == other.y and self.z == other.z
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+    def __pow__(self, power):
+        return Vector(self.x**power, self.y**power, self.z**power)
+
+
+v1 = Vector(1, 2, 3)
+v2 = Vector(4, 5, 6)
+print(v1)
+print(len(v1))
+
+v3 = v1 + v2
+print(v3)
+
+v4 = v1 - v2
+print(v4)
+
+v5 = v1 * 2
+print(v5)
+
+v6 = v1 * v2
+print(v6)
+
+print(v1 == v2)
+print(v1 != v2)
+
+v7 = v1 ** 2
+print(v7)
+
+v1 += v2
+print(v1)
+
+v2 -= v1
+print(v2)
+
+v3 *= 2
+print(v3)
