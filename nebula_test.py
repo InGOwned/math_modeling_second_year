67295a6b9616a7d304c2c0c6da8056be7c49f11d
InGOwned <103064059+InGOwned@users.noreply.github.com>
2024-01-27 10:28:02 +0000

    .

--- nebula_test.py
+++ nebula_test.py
@@ -13,19 +13,24 @@ def grahamscan(A):
     '''Алгоритм Грэхема'''
     n = len(A)  # число точек
     P = list(range(n))  # список номеров точек
+
     for i in range(1, n):
         if A[P[i]][0] < A[P[0]][0]:  # если P[i]-ая точка лежит левее P[0]-ой точки
             P[i], P[0] = P[0], P[i]  # меняем местами номера этих точек
+    
     for i in range(2, n):  # сортировка
         j = i
         while j > 1 and (rotate(A[P[0]], A[P[j - 1]], A[P[j]]) < 0):
             P[j], P[j - 1] = P[j - 1], P[j]
             j -= 1
+    
     S = [P[0], P[1]]  # стек
+
     for i in range(2, n):
         while rotate(A[S[-2]], A[S[-1]], A[P[i]]) < 0:
             del S[-1]
         S.append(P[i])
+    
     return S
 
 
